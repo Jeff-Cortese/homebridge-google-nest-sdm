4a231059aed7baaed332946853f364cdaaff58c8
Jeff-Cortese <8184497+Jeff-Cortese@users.noreply.github.com>
2025-03-01 11:11:04 -0600

    instructions

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Fork of [homebridge-google-nest-sdm](https://github.com/potmat/homebridge-google-nest-sdm)
 
 - Only supports nest thermostat
-- Adds helper script to generate refresh token
+- Adds [helper script](./get_refresh_token.sh) to generate refresh token. 
+  - This requires a "Desktop App" Google OAuth client.
+  - Ensure that `CLIENT_ID` and `CLIENT_SECRET` are set in your terminal session.
+  - NOTE: The refresh_token will expire in seven days when the [`Google Auth Platform > Audience > Publishing Status`](https://console.cloud.google.com/auth/audience) is in test mode. Publishing the app removes the expiration (This will not remove expiration from an existing token. You must regenerate the refresh_token one time after turning off test mode)
--- get_refresh_token.sh
+++ get_refresh_token.sh
@@ -15,8 +15,13 @@ SCOPES="https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fsdm.service+https%3A%2F%2Fwww.
 
 # Step 1: Generate Authorization URL
 echo "Open the following URL in your browser and authorize the application:"
+echo "1. You will be redirected to a page to choose/login to your google account."
+echo "2. After you login, you will be redirected to a page that says 'Google hasn't verified this app'. This is expected. Click 'Advanced' and then 'Go to <Your App Name> (unsafe)'"
+echo "3. Next, you will be asked to grant permission to the application. Select All, and continue."
+echo "4. Finally, you will be redirected to a an empty page with localhost in the URL. This is expected."
+echo '5. Copy the authorization code parameter from your browsers address bar and paste it below. The parameter will be in the form of http://localhost/?code=......&" Copy everything between the = and &. Do not include the = or &.'
 echo ""
-echo "https://accounts.google.com/o/oauth2/v2/auth?client_id=$CLIENT_ID&response_type=code&redirect_uri=$REDIRECT_URI&scope=$SCOPES&access_type=offline&prompt=consent"
+echo "URL: https://accounts.google.com/o/oauth2/v2/auth?client_id=$CLIENT_ID&response_type=code&redirect_uri=$REDIRECT_URI&scope=$SCOPES&access_type=offline&prompt=consent"
 echo ""
 read -p "Enter the authorization code: " AUTH_CODE
 
